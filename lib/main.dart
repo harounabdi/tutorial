3b92856d1fdad1b910171243140852aa33e6c8f0
harounabdi <125971987+harounabdi@users.noreply.github.com>
2023-12-06 14:08:34 +0300

    Tiktok Clone making by flutter

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:wacomments/homeScreen.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TikTokClone(),
+      debugShowCheckedModeBanner: false, // Set this to false to remove the debug banner
+    );
+  }
+}
